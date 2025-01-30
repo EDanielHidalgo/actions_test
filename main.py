602b0e0f0f4cf90675d05f04d085e163d1ec9e31
Daniel Hidalgo <e.daniel.hidalgo@gmail.com>
2025-01-30 13:20:31 -0600

    Checking failure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def return_failure():
+    return False
+
+
 def return_success():
     return True
 
 
 if __name__ == '__main__':
-    return_success()
+    # return_success()
+    return_failure()
